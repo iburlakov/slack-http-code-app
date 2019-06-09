184f79a367f9083a74152fe5a305a44f8946428b
iburlakov <iburlakov@gmail.com>
2019-06-09 21:34:35 +0300

    Some clean up

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotEnv = require('dotenv');
 const cors = require('cors');
 
 const getCodeInfo = require('./src/codeInfoService');
